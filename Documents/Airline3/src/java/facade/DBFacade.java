00819bfce370df8faa17cc3fe509e229a6169b14
Joachim Dittman <joachimdj@me.com>
2015-05-13 12:59:50 +0200

    Delete are working

---
+++ Documents/Airline3/src/java/facade/DBFacade.java
@@ -0,0 +1,82 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package facade;
+
+import Entity.*;
+import java.util.ArrayList;
+import java.util.List;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+import javax.persistence.Query;
+
+/**
+ *
+ * @author Michael
+ */
+public class DBFacade implements DBInterface{
+
+    private EntityManagerFactory emf = Persistence.createEntityManagerFactory("CA2");
+    private EntityManager em = emf.createEntityManager();
+
+    private static DBFacade instance = null;
+
+    public DBFacade() {
+
+    }
+ 
+
+     @Override
+    public Reservation getReservation(Long id) {
+        Reservation found = em.find(Reservation.class, id);
+        return found;
+    }
+    @Override
+    public List<Flight> getAllFlights(String startAirport, Long departuredate) {
+        List<Flight> allFlights = new ArrayList();
+        Query query = em.createQuery("SELECT e FROM Flight e WHERE e.destination<='"+startAirport+"' AND e.departuredate>='"+departuredate+"'");
+        allFlights = query.getResultList();
+        return allFlights;
+    }
+
+    @Override
+    public List<Flight> getAllFlights2(String startAirport,String endAirport,String date) {
+        List<Flight> allFlights = new ArrayList();
+        Query query = em.createQuery("SELECT e FROM Flight e WHERE e.destination='"+startAirport+"' AND e.endAirport='"+endAirport+"' AND e.departuredate LIKE '"+date+"'");
+        allFlights = query.getResultList();
+        return allFlights;
+    }
+    
+    @Override
+    public void deleteReservation(Long id){
+         
+        Query query = em.createQuery("DELETE FROM Reservation p WHERE p.reservationId = "+id);
+//      
+        em.getTransaction().begin();
+        query.executeUpdate();
+        em.getTransaction().commit();
+        
+        
+    }
+    
+    @Override
+    public Reservation CreateReservation(Passengers passengerObjects, long flightID) {
+        em.getTransaction().begin(); 
+     //   em.merge(p);
+        em.getTransaction().commit();
+        return null;
+    }
+/*
+    @Override
+    public void updateReservation(Reservation p) {
+        em.getTransaction().begin();
+        em.merge(p);
+        em.getTransaction().commit();
+    }
+*/
+ 
+ 
+}
